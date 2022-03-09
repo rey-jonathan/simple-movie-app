374650ce34f7ad41f2294f85ccdad33fbbd50838
Rey Gienaldo Jonathan <rey.jonathan@kiranatama.com>
2022-03-09 09:44:40 +0700

    Add navigation for SPA

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import MovieList from "./components/MovieList";
+import MyMovie from "./components/MyMovie";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <Router>
+      <div>
+        <label>
+          <Link to='/'>Movie List</Link>
+        </label>
+        <label>
+          <Link to='/my-movie'>My Movie</Link>
+        </label>
+      </div>
+
+      <Routes>
+        <Route exact path='/' element={<MovieList />} />
+        <Route path='my-movie' element={<MyMovie />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/MyMovie.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyMovie = () => {
+  return <div>My Movie List</div>;
+};
+
+export default MyMovie;
